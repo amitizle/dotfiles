a830152f740c7ad32bab4c7a70182864024bd083
Amit <amit.goldberg@gmail.com>
2020-09-12 16:50:06 +0300

    remove a lot of things, add Brewfile

--- installation_scripts/ngrok.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO this is not yet implemented REALLY
-
-curl -L "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip" -o /tmp/ngrok.zip
-unzip -j -d "$HOME"/.apps/bin /tmp/ngrok.zip
